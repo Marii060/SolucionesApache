30b15ccf423c93cac4bbe480448180276978b30a
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-05-14 20:58:03 -0500

    Completar modelos de Inventario y configurar panel administrativo

--- inventario/admin.py
+++ inventario/admin.py
@@ -14,7 +14,7 @@ class CategoriaAdmin(admin.ModelAdmin):
 
 @admin.register(Proveedor)
 class ProveedorAdmin(admin.ModelAdmin):
-    list_display = ('razon_social', 'identificacion', 'telefono', 'correo', 'activo')
+    list_display = ('nombre', 'razon_social', 'identificacion', 'telefono', 'correo', 'activo')
     list_filter = ('activo',)
     search_fields = ('razon_social', 'identificacion')
 
@@ -24,7 +24,7 @@ class ProductoAdmin(admin.ModelAdmin):
     list_display = ('codigo_interno', 'nombre', 'id_categoria', 'precio_venta', 'cantidad', 'disponible')
     # Filtros laterales por categoría, marca y disponibilidad
     list_filter = ('id_categoria', 'id_marca', 'disponible')
-    # Buscador por nombre y códigos
+    # Busca por nombre y códigos
     search_fields = ('nombre', 'codigo_interno', 'codigo_barras')
-    # Esto permite seleccionar los proveedores de forma más cómoda en la relación Muchos a Muchos
+    # permite seleccionar los proveedores de forma más cómoda en la relación Muchos a Muchos
     filter_horizontal = ('proveedores',)
