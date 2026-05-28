e92639b4a8d376e41c3bd1a0d6dd4256249c2d9e
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-05-27 20:44:35 -0500

    Configurar panel de administracion de gestion para Clientes y Motos con inlines al igual realice comentarios

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 from .models import Cliente, Moto
 
-admin.site.register(Cliente)
-admin.site.register(Moto)
+# Esto permite que al abrir un cliente, veas y agregues sus motos ahí mismo.
+class MotoInline(admin.TabularInline):
+    model = Moto
+    extra = 1 # Muestra una fila en blanco lista para agregar una moto
+
+# 2. SECCIÓN DE CLIENTES
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    # Qué columnas queremos ver en la lista principal
+    list_display = ('numero_documento', 'razon_social', 'nombre', 'correo', 'telefono')
+    
+    # Barra de búsqueda (puedes buscar por cédula, nombre, etc.)
+    search_fields = ('numero_documento', 'razon_social', 'nombre', 'correo')
+    
+    # Le conectamos el "Inline" de las motos
+    inlines = [MotoInline]
+
+# 3. SECCIÓN DE MOTOS (Vista Individual)
+# Por si quieres ver el listado de TODAS las motos del taller
+@admin.register(Moto)
+class MotoAdmin(admin.ModelAdmin):
+    list_display = ('placa', 'marca', 'modelo', 'cilindraje', 'id_cliente')
+    list_filter = ('marca',) # Filtro lateral para buscar por marca (Yamaha, Honda, etc.)
+    
+    # Permite buscar la placa de la moto o el nombre de su dueño
+    search_fields = ('placa', 'id_cliente__razon_social', 'id_cliente__nombre')
