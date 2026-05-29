c48494300f86cb6fcdf06f97012f0c33256f086b
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-05-28 19:14:51 -0500

    Cree todas las carpetas de templates, el template que va en la raiz cree el archivo base.html, tambien en gestion en views.py use un candado de Django que evita que cualquier persona sin cuenta pueda ver esta información

--- gestion/views.py
+++ gestion/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Cliente
 
-# Create your views here.
+# Este candado asegura que nadie sin cuenta pueda entrar aquí
+@login_required 
+def lista_clientes(request):
+    # Traemos todos los clientes de la base de datos
+    clientes = Cliente.objects.all() 
+    
+    # Se los enviamos al HTML
+    return render(request, 'gestion/lista_clientes.html', {'clientes': clientes})
