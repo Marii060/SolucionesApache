22122d52130b91a21142f6f61a201a4d7d6dc67f
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-05-28 19:56:43 -0500

    en config en urls.py active el sistema de login seguro de Django

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include # <-- Importante agregar include aquí
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Aquí activamos todo el sistema de Login/Logout seguro de Django
+    path('cuentas/', include('django.contrib.auth.urls')),
     # Conectamos las rutas de gestión
     path('gestion/', include('gestion.urls')), 
 ]
