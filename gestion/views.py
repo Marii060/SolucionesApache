f99b802c39a9591af29b8f11852f883e39a26c01
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-05-29 19:05:50 -0500

    en gestion/views.py implemente El Cerebro del Dashboard la vista que irá a la base de datos, contará los registros y se los pasará a la pantalla.

--- gestion/views.py
+++ gestion/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import Cliente
+from .models import Cliente, Moto # Perfecto, aquí tienes los modelos
 
-# Este candado asegura que nadie sin cuenta pueda entrar aquí
+@login_required
+def dashboard(request):
+    # Va a la DB y cuenta cuántos clientes hay
+    total_clientes = Cliente.objects.count()
+    
+    # Va a la DB y cuenta cuántas motos hay
+    total_motos = Moto.objects.count() 
+    
+    contexto = {
+        'total_clientes': total_clientes,
+        'total_motos': total_motos,
+    }
+    # Renderizamos la pantalla enviando el contexto de la base de datos
+    return render(request, 'gestion/dashboard.html', contexto)
+
 @login_required 
 def lista_clientes(request):
     # Traemos todos los clientes de la base de datos
-    clientes = Cliente.objects.all() 
-    
+    clientes = Cliente.objects.all()   
     # Se los enviamos al HTML
     return render(request, 'gestion/lista_clientes.html', {'clientes': clientes})
