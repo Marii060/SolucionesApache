45e80ff3373140b4e3b5ff912fadabda4a8cbd2c
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-06-10 21:28:52 -0500

    feat(operaciones): crear vista crear_servicio con asignación automática del usuario en sesión

--- operaciones/views.py
+++ operaciones/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import ServicioForm
 
-# Create your views here.
+@login_required
+def crear_servicio(request):
+    if request.method == 'POST':
+        form = ServicioForm(request.POST)
+        if form.is_valid():
+            # form.save(commit=False) prepara los datos pero pausa el guardado en la BD
+            servicio = form.save(commit=False) 
+            
+            # Asignamos automáticamente el usuario que tiene la sesión iniciada
+            servicio.id_usuario = request.user 
+            
+            # Ahora sí, guardamos definitivamente en MySQL
+            servicio.save() 
+            
+            messages.success(request, '¡Orden de servicio creada con éxito!')
+            return redirect('operaciones:lista_servicios') # Ajusta el nombre de tu URL si es diferente
+    else:
+        form = ServicioForm()
+        
+    return render(request, 'operaciones/registrar_servicio.html', {'form': form})
