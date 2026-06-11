cff77a9567c814626deede6a7f0b3d72bf69d8d4
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-06-10 21:41:47 -0500

    feat(operaciones): configurar rutas urls para el módulo de servicios

---
+++ operaciones/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'operaciones' # Esto es clave para que Django sepa de qué app es la URL
+
+urlpatterns = [
+    # Ruta para la pantalla que acabamos de crear
+    path('servicios/nuevo/', views.crear_servicio, name='crear_servicio'),
+]
