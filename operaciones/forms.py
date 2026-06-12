529236cb18c60eb06a77b408b9b57de64cb1e123
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-06-12 16:12:35 -0500

    crear formularios para registrar catálogo de servicios

--- operaciones/forms.py
+++ operaciones/forms.py
@@ -1,5 +1,22 @@
 from django import forms
-from .models import Servicio
+from .models import Servicio, ListaServicio
+
+class ListaServicioForm(forms.ModelForm):
+    class Meta:
+        model = ListaServicio
+        fields = ['nombre_servicio', 'precio_base_mano_obra', 'estado', 'descripcion']
+        labels = {
+            'nombre_servicio': 'Nombre Servicio',
+            'precio_base_mano_obra': 'Precio Base Mano Obra',
+            'estado': 'Estado',
+            'descripcion': 'Descripción',
+        }
+        widgets = {
+            'nombre_servicio': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ej: Mantenimiento General'}),
+            'precio_base_mano_obra': forms.NumberInput(attrs={'class': 'form-control'}),
+            'estado': forms.CheckboxInput(attrs={'class': 'form-check-input', 'style': 'transform: scale(1.5); margin-top: 6px;'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
 
 class ServicioForm(forms.ModelForm):
     class Meta:
