64d83ea5ab9c052b02d93f46122a5159db100de8
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-06-12 16:13:46 -0500

    configurar rutss para el módulo de taller y catálogo de servicios

--- operaciones/urls.py
+++ operaciones/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'operaciones' # Esto es clave para que Django sepa de qué app es la URL
 
 urlpatterns = [
-    # Ruta para la pantalla que acabamos de crear
     path('servicios/nuevo/', views.crear_servicio, name='crear_servicio'),
+    #rutas para el catálogo de servicios
+    path('catalogo/', views.lista_catalogo, name='lista_catalogo'),
+    path('catalogo/nuevo/', views.crear_catalogo, name='crear_catalogo'),
 ]
