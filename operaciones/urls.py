5902896e8002a1eea2ce921cf68aa36106708ac8
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-06-12 17:07:50 -0500

    en urls agregamos las rutas de editar y eliminar

--- operaciones/urls.py
+++ operaciones/urls.py
@@ -4,8 +4,11 @@ from . import views
 app_name = 'operaciones' # Esto es clave para que Django sepa de qué app es la URL
 
 urlpatterns = [
-    path('servicios/nuevo/', views.crear_servicio, name='crear_servicio'),
-    #rutas para el catálogo de servicios
+     #rutas para el catálogo de servicios
     path('catalogo/', views.lista_catalogo, name='lista_catalogo'),
     path('catalogo/nuevo/', views.crear_catalogo, name='crear_catalogo'),
+    path('catalogo/editar/<int:id>/', views.editar_catalogo, name='editar_catalogo'),
+    path('catalogo/eliminar/<int:id>/', views.eliminar_catalogo, name='eliminar_catalogo'),
+    #rutas para servicios que presta el taller
+    path('servicios/nuevo/', views.crear_servicio, name='crear_servicio'),
 ]
