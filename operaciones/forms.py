8932c15a34cd483228da6a7a354b3586f24c09de
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-06-18 11:39:50 -0500

    agregue un nuevo campo de mecanico

--- operaciones/forms.py
+++ operaciones/forms.py
@@ -22,16 +22,18 @@ class ServicioForm(forms.ModelForm):
     class Meta:
         model = Servicio
         # Solo pedimos lo necesario para abrir la orden
-        fields = ['codigo_servicio', 'id_moto', 'descripcion', 'estado']
+        fields = ['codigo_servicio', 'id_moto', 'mecanico', 'descripcion', 'estado']
         labels = {
             'codigo_servicio': 'Código de la Orden (Ej: ORD-001)',
             'id_moto': 'Seleccione la Moto (Placa)',
+            'mecanico':'Mecánico Asignado',
             'descripcion': 'Descripción de la Falla / Motivo de Ingreso',
             'estado': 'Estado Inicial',
         }
         widgets = {
             'codigo_servicio': forms.TextInput(attrs={'class': 'form-control'}),
             'id_moto': forms.Select(attrs={'class': 'form-select'}),
+            'mecanico': forms.Select(attrs={'class': 'form-control'}),
             'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
             'estado': forms.Select(attrs={'class': 'form-select'}),
         }
