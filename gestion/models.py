24f60822f08442a672ef9e53b2a7728c8f87f397
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-06-21 20:51:15 -0500

    en gestion/model.py agregue la propiedad ultimo_servicio al modelo Moto para que en lista_motos fuera funcional el ultimo_servicio

--- gestion/models.py
+++ gestion/models.py
@@ -30,6 +30,10 @@ class Moto(models.Model):
     kilometraje = models.CharField(max_length=30, null=True, blank=True)
     id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, db_column='id_cliente')
     observaciones = models.TextField(null=True, blank=True)
+    
+    @property
+    def ultimo_servicio(self):
+        return self.servicio_set.order_by('-fecha_inicio').first()
     def __str__(self):
         return f"{self.placa} - {self.modelo}"
 
