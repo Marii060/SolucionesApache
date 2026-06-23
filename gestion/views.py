56394d3a723b8310a4c80983b88f3855bd60ce33
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-06-23 10:50:51 -0500

    fix: corregir renderizado de configuración

--- gestion/views.py
+++ gestion/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import login_required, user_passes_test
 import json
 import datetime
@@ -13,19 +12,13 @@ from gestion.models import Cliente, Moto, Log, ConfiguracionSistema
 from inventario.models import MarcaProducto, Categoria, Proveedor, Producto, MovimientoInventario, Compra, DetalleCompra
 from operaciones.models import ListaServicio, Servicio, DetalleServicio, Venta, DetalleVenta, Credito, CreditoPagado
 from .forms import ClienteForm, MotoForm, ConfiguracionSistemaForm
-from operaciones.models import Credito, CreditoPagado
 
 def personal_required(user):
     return user.is_staff
 
-#Aplicación en la vista del dashboard
+#DASHBOARD
 @login_required
-@user_passes_test(personal_required, login_url='login') # login_url evita errores si no tiene permiso
-def dashboard(request):
-    # Aquí va toda la lógica de tu dashboard (consultas a la BD, render, etc.)
-    return render(request, 'gestion/dashboard.html', {})
-
-@login_required
+@user_passes_test(personal_required, login_url='login')
 def dashboard(request):
     total_clientes = Cliente.objects.count()
     total_motos = Moto.objects.count() 
@@ -42,6 +35,7 @@ def dashboard(request):
     }
     return render(request, 'gestion/dashboard.html', contexto)
 
+#CLIENTES
 @login_required
 def lista_clientes(request):
     clientes = Cliente.objects.annotate(cantidad_motos=Count('moto')).order_by('-id_cliente')
@@ -55,21 +49,19 @@ def lista_clientes(request):
             Q(numero_documento__icontains=buscar)
         )
     if tipo:
-        # Esto filtrará los clientes donde el campo razon_social coincida con el valor enviado
         clientes = clientes.filter(razon_social=tipo)
- 
+
     paginator = Paginator(clientes, 8) 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
     return render(request, 'gestion/lista_cliente.html', {
         'page_obj': page_obj,
-        'buscar': buscar, # Útil para mantener la búsqueda al cambiar de página
+        'buscar': buscar,
         'tipo': tipo,
         'estado': estado
     })
 
-# Crear cliente
 @login_required
 def crear_cliente(request):
     if request.method == 'POST':
@@ -82,7 +74,6 @@ def crear_cliente(request):
         form = ClienteForm()
     return render(request, 'gestion/crear_cliente.html', {'form': form})
 
-# Detalle de cliente
 @login_required
 def detalle_cliente(request, cliente_id):
     cliente = get_object_or_404(Cliente, pk=cliente_id)
@@ -100,17 +91,14 @@ def editar_cliente(request, cliente_id):
     cliente = get_object_or_404(Cliente, pk=cliente_id)
     
     if request.method == 'POST':
-        # Instanciamos el formulario con los datos enviados y la instancia del cliente a editar
         form = ClienteForm(request.POST, instance=cliente)
         if form.is_valid():
             form.save()
             messages.success(request, '¡Cliente actualizado correctamente!')
             return redirect('gestion:lista_cliente')
     else:
-        # Cargamos el formulario con los datos actuales del cliente para que aparezcan en el HTML
         form = ClienteForm(instance=cliente)
     
-    # Renderizamos con el nombre de archivo correcto y la variable 'editando' en True
     return render(request, 'gestion/crear_cliente.html', {
         'form': form, 
         'editando': True 
@@ -125,9 +113,9 @@ def eliminar_cliente(request, cliente_id):
         return redirect('gestion:lista_cliente')
     return render(request, 'gestion/eliminar.html', {'cliente': cliente})
 
+#MOTOS
 @login_required
 def registrar_moto(request, id_cliente=None):
-    # Si viene un id_cliente, buscamos al cliente para pre-seleccionarlo
     cliente_inicial = None
     if id_cliente:
         cliente_inicial = get_object_or_404(Cliente, pk=id_cliente)
@@ -135,24 +123,16 @@ def registrar_moto(request, id_cliente=None):
     if request.method == 'POST':
         form = MotoForm(request.POST)
         if form.is_valid():
-            #Detenemos el guardado directo en la BD
             moto = form.save(commit=False)
-            
-            # Si venimos desde el perfil de un cliente, le inyectamos el dueño manualmente
             if cliente_inicial:
                 moto.id_cliente = cliente_inicial
-                
-            # Ahora sí guardamos de forma segura en la base de datos
             moto.save()
-            
             messages.success(request, '¡Motocicleta registrada con éxito!')
             
-            # Si venía de un perfil de cliente, volvemos allí; si no, a la lista general
             if id_cliente:
                 return redirect('gestion:detalle_cliente', cliente_id=id_cliente)
             return redirect('gestion:lista_motos')
     else:
-        # Pre-llenamos el formulario si tenemos el cliente
         initial_data = {'id_cliente': cliente_inicial} if cliente_inicial else None
         form = MotoForm(initial=initial_data)
 
@@ -166,10 +146,10 @@ def registrar_moto(request, id_cliente=None):
         'form': form, 
         'cliente': cliente_inicial
     })
+
 @login_required
 def lista_motos(request):
-    motos_list = Moto.objects.all().order_by('-id_moto') # Las más recientes primero
-    # Lógica de búsqueda
+    motos_list = Moto.objects.all().order_by('-id_moto')
     buscar = request.GET.get('buscar')
     if buscar:
         motos_list = motos_list.filter(
@@ -177,19 +157,19 @@ def lista_motos(request):
             Q(marca__icontains=buscar) |
             Q(id_cliente__nombre__icontains=buscar)
         )
-    # Paginación
+    
     paginator = Paginator(motos_list, 7) 
     page_number = request.GET.get('page')
     motos = paginator.get_page(page_number)
     
     return render(request, 'gestion/lista_motos.html', {'motos': motos})
 
-# Detalle moto
+@login_required
 def detalle_moto(request, pk):
     moto = get_object_or_404(Moto, pk=pk)
     return render(request, 'gestion/detalle_moto.html', {'moto': moto})
 
-# Editar moto
+@login_required
 def editar_moto(request, pk):
     moto = get_object_or_404(Moto, pk=pk)
     if request.method == 'POST':
@@ -199,7 +179,6 @@ def editar_moto(request, pk):
             return redirect('gestion:lista_motos')
     else:
         form = MotoForm(instance=moto)
-        # Aplicamos estilos a los campos del formulario
         for field in form.fields.values():
             if isinstance(field.widget, forms.Select):
                 field.widget.attrs.update({'class': 'form-select'})
@@ -208,7 +187,7 @@ def editar_moto(request, pk):
                 
     return render(request, 'gestion/registrar_moto.html', {'form': form, 'moto': moto})
 
-# Eliminar moto
+@login_required
 def eliminar_moto(request, pk):
     moto = get_object_or_404(Moto, pk=pk)
     if request.method == 'POST':
@@ -216,6 +195,7 @@ def eliminar_moto(request, pk):
         return redirect('gestion:lista_motos')
     return render(request, 'gestion/eliminar_moto.html', {'moto': moto})
 
+#CONFIGURACIÓN Y BACKUPS
 @login_required
 def panel_configuracion(request):
     config = ConfiguracionSistema.obtener_config()
@@ -224,9 +204,9 @@ def panel_configuracion(request):
         form = ConfiguracionSistemaForm(request.POST, instance=config)
         if form.is_valid():
             form.save()
-            messages.success(request, '¡Configuración de Soluciones Apache actualizada correctamente!')
-            return redirect('gestion:panel') # Ajusta esto al nombre de tu URL
-    else:
+            messages.success(request, '¡Configuración del Taller actualizada correctamente!')
+            return redirect('gestion:panel')
+    else: 
         form = ConfiguracionSistemaForm(instance=config)
     
     return render(request, 'gestion/panel_configuracion.html', {
@@ -238,7 +218,7 @@ def panel_configuracion(request):
 def crear_backup_manual(request):
     if not request.user.is_superuser:
         messages.error(request, 'No tienes permisos para realizar copias de seguridad.')
-        return redirect('panel_configuracion')
+        return redirect('gestion:panel')
 
     modelos_a_respaldar = [
         ConfiguracionSistema,
@@ -259,7 +239,7 @@ def crear_backup_manual(request):
         Credito,          
         CreditoPagado,    
         Log,              
-        ]
+    ]
     
     data_consolidada = []
     for modelo in modelos_a_respaldar:
