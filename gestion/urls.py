239760fe42f52525a4d6dbc4c52781fc5ca9ac74
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-07-04 19:01:26 -0500

    Configurar rutas de login y logout personalizadas en la app gestión

--- gestion/urls.py
+++ gestion/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'gestion'
 
 urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('', views.dashboard, name='dashboard'), 
     path('clientes/', views.lista_clientes, name='lista_cliente'), 
     path('clientes/nuevo/', views.crear_cliente, name='crear_cliente'),
