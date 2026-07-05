07b4b30f07e7891577c10a77f5676efeb952e122
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-07-04 19:13:01 -0500

    Corregir error de redirección en admin/logout añadiendo alias para la ruta login

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', RedirectView.as_view(url='/gestion/login/', permanent=False), name='login'),
     # Conectamos las rutas de gestión
     path('gestion/', include('gestion.urls')), 
     path('operaciones/', include('operaciones.urls')), 
