21cb6263476bebf74fe654f11d95535d9aae10d8
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-07-04 23:05:49 -0500

    Agregar campo 'estado' a MarcaProducto y actualizar migraciones para permitir habilitar/deshabilitar

--- inventario/models.py
+++ inventario/models.py
@@ -5,6 +5,7 @@ class MarcaProducto(models.Model):
     id_marca = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100, unique=True)
     descripcion = models.TextField(blank=True, null=True)
+    estado = models.BooleanField(default=True)  
     
     class Meta:
         verbose_name = "Marca de Producto"
