9582e1abfc0c429603f5037ba74eb67dce7a8e2f
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-07-06 00:04:18 -0500

    Actualizar ServicioForm para incluir campo mecánico como TextInput

--- operaciones/forms.py
+++ operaciones/forms.py
@@ -21,19 +21,18 @@ class ListaServicioForm(forms.ModelForm):
 class ServicioForm(forms.ModelForm):
     class Meta:
         model = Servicio
-        # Solo pedimos lo necesario para abrir la orden
         fields = ['codigo_servicio', 'id_moto', 'mecanico', 'descripcion', 'estado']
         labels = {
             'codigo_servicio': 'Código de la Orden (Ej: ORD-001)',
             'id_moto': 'Seleccione la Moto (Placa)',
-            'mecanico':'Mecánico Asignado',
+            'mecanico': 'Mecánico Asignado',
             'descripcion': 'Descripción de la Falla / Motivo de Ingreso',
             'estado': 'Estado Inicial',
         }
         widgets = {
             'codigo_servicio': forms.TextInput(attrs={'class': 'form-control'}),
             'id_moto': forms.Select(attrs={'class': 'form-select'}),
-            'mecanico': forms.Select(attrs={'class': 'form-control'}),
+            'mecanico': forms.TextInput(attrs={'class': 'form-control'}), 
             'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
             'estado': forms.Select(attrs={'class': 'form-select'}),
         }
