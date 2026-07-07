b4e4e50cc0cb4a83e0f93a16a887ad58d1bf1426
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-07-07 10:46:53 -0500

    Actualizar settings y requerimientos para producción

--- config/settings.py
+++ config/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(8udh@l59q2t6$oaaq#c2o+r#-tgbjph)(8+d7u%)kxq%v(zys'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
