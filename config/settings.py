e355ab00106009a7a9ac9a778936080e4e88d6d4
Marii060 <157556160+Marii060@users.noreply.github.com>
2026-07-07 19:29:53 -0500

    Crear página de error 404 personalizada para mejor experiencia de usuario

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-(8udh@l59q2t6$oaaq#c2o+r#-tgbjph)(8+d7u%)kxq%v(zys
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
